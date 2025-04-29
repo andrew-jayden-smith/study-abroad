2b930ea3786a7b244d66a1ee77758e7c26129efb
Andrew Smith <smithdrew867@gmail.com>
2025-04-29 00:43:34 -0400

    Redesign of university logos, header, footer

--- study-abroad/Program.cs
+++ study-abroad/Program.cs
@@ -7,9 +7,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://api.postmarkapp.com/")});
-builder.Services.AddSingleton(new ContactUsService(""));
+builder.Services.AddSingleton(new ContactUsService("081e5b46-22d2-4f9f-b6ac-d18d41d11e8a"));
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
-
