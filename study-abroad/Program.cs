02bef274c05ca0f059d3338a6be444b5d949ffb8
Andrew Smith <smithdrew867@gmail.com>
2025-04-29 11:35:01 -0400

    API Token Fix

--- study-abroad/Program.cs
+++ study-abroad/Program.cs
@@ -7,7 +7,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://api.postmarkapp.com/")});
-builder.Services.AddSingleton(new ContactUsService("081e5b46-22d2-4f9f-b6ac-d18d41d11e8a"));
+builder.Services.AddSingleton(new ContactUsService(""));
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
